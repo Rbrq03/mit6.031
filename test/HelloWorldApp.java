fb8b8edb4e83c88e70f3b3fd283807382c723a4d
Rbrq03 <Rbrq03@outlook.com>
2023-01-12 01:26:14 +0800

    public commit

--- test/HelloWorldApp.java
+++
@@ -1,9 +0,0 @@
-/** 
- * The application simply print
- * Hello world 
- */
-public class HelloWorldApp{
-    public static void main(String[] args){
-        System.out.println("Hello world");
-    }
-}
